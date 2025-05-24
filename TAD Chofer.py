ed32c99ef91adb815958828297d42cf467cf138a
Dekirucos <Axelespeche438@gmail.com>
2025-05-24 17:18:39 -0300

    Update TAD Chofer.py

--- TAD Chofer.py
+++ TAD Chofer.py
@@ -1,55 +1,26 @@
 def crearChofer():
-    return {
-        "nombre": "",
-        "apellido": "",
-        "legajo": 0,
-        "fecha_ingreso": "",
-        "numero_camion": "",
-        "zona_trabajo": ""
-    }
+    return ["", "", 0, "", "", ""]
 
 def cargarChofer(chofer, nombre, apellido, legajo, fecha_ingreso, numero_camion, zona_trabajo):
-    chofer["nombre"] = nombre
-    chofer["apellido"] = apellido
-    chofer["legajo"] = legajo
-    chofer["fecha_ingreso"] = fecha_ingreso
-    chofer["numero_camion"] = numero_camion
-    chofer["zona_trabajo"] = zona_trabajo
+    chofer[0] = nombre
+    chofer[1] = apellido
+    chofer[2] = legajo
+    chofer[3] = fecha_ingreso
+    chofer[4] = numero_camion
+    chofer[5] = zona_trabajo
 
-def verNombre(chofer):
-    return chofer["nombre"]
+# Ver funciones
+def verNombre(chofer): return chofer[0]
+def verApellido(chofer): return chofer[1]
+def verLegajo(chofer): return chofer[2]
+def verFechaIngreso(chofer): return chofer[3]
+def verNumeroCamion(chofer): return chofer[4]
+def verZonaTrabajo(chofer): return chofer[5]
 
-def verApellido(chofer):
-    return chofer["apellido"]
-
-def verLegajo(chofer):
-    return chofer["legajo"]
-
-def verFechaIngreso(chofer):
-    return chofer["fecha_ingreso"]
-
-def verNumeroCamion(chofer):
-    return chofer["numero_camion"]
-
-def verZonaTrabajo(chofer):
-    return chofer["zona_trabajo"]
-
-def modificarChofer(chofer):
-    nombre = input("Nuevo nombre (Enter para mantener): ")
-    apellido = input("Nuevo apellido (Enter para mantener): ")
-    fecha_ingreso = input("Nueva fecha de ingreso (AAAA-MM-DD) (Enter para mantener): ")
-    numero_camion = input("Nuevo número de camión (Enter para mantener): ")
-    zona = input("Nueva zona de trabajo (Enter para mantener): ")
-
-    if nombre == "":
-        nombre = verNombre(chofer)
-    if apellido == "":
-        apellido = verApellido(chofer)
-    if fecha_ingreso == "":
-        fecha_ingreso = verFechaIngreso(chofer)
-    if numero_camion == "":
-        numero_camion = verNumeroCamion(chofer)
-    if zona == "":
-        zona = verZonaTrabajo(chofer)
-
-    cargarChofer(chofer, nombre, apellido, verLegajo(chofer), fecha_ingreso, numero_camion, zona)
+# Modificar funciones
+def modNombre(chofer, nuevo): chofer[0] = nuevo
+def modApellido(chofer, nuevo): chofer[1] = nuevo
+def modLegajo(chofer, nuevo): chofer[2] = nuevo
+def modFechaIngreso(chofer, nuevo): chofer[3] = nuevo
+def modNumeroCamion(chofer, nuevo): chofer[4] = nuevo
+def modZonaTrabajo(chofer, nuevo): chofer[5] = nuevo
